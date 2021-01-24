5aab009916aeb49763d1b4708baff29f20e033b0
Michael P Nance <michael.p.nance@gmail.com>
2021-01-23 23:02:32 -0500

    Random board generation is done. Still need board validation logic.

--- gen.js
+++ gen.js
@@ -20,10 +20,6 @@ const shuffle = (arr, shuffled = []) => {
 const resources = [...desert, ...lumber, ...grain, ...brick, ...wool, ...ore];
 const values = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12];
 
-const map = matrix(5, 5).map((x, i) =>
-  x.map((_, j) => (i + j > 1 && i + j < 7 ? [i, j] : [0, 0]))
-);
-
 // Tuples are the coordinate offsets for every
 // neighboring hex
 const edge = [
@@ -60,5 +56,44 @@ const position = (hex, vertex, map) => [
   neighbor(hex, vertex[1], map),
 ];
 
-console.log(map.flat());
-console.log(unflat(5, 5, map.flat()));
+const map = matrix(5, 5).map((x, i) =>
+  x.map((_, j) =>
+    i + j > 1 && i + j < 7
+      ? { type: null, value: null }
+      : { type: "X", value: 0 }
+  )
+);
+
+// Use this to apply shuffled array of resource types or values
+// to the board.  Hexes should be a flattened array representation
+// of the board.  Returns new flattened array representation of board.
+// NOTE: If you reapply resources then you have to reapply values.
+const applyItems = (items, field, hexes) => {
+  if (items.length === 0) return hexes;
+  const i = hexes.findIndex((hex) => hex[field] === null);
+  // in case of desert hex, set value to 0
+  const updatedHex =
+    items[0] === "D"
+      ? { type: "D", value: 0 }
+      : { ...hexes[i], [field]: items[0] };
+  const updatedHexes = [
+    ...hexes.slice(0, i),
+    updatedHex,
+    ...hexes.slice(i + 1),
+  ];
+  return applyItems(items.slice(1), field, updatedHexes);
+};
+
+// Returns true if hex and neighbor have the same resource type
+// const checkNeighbor = (hex, edge, map) => {
+//   const [x, y] = hex;
+//   return map[x][y].type === neighbor(hex, edge, map).type;
+// };
+
+const temp = applyItems(
+  shuffle(values),
+  "value",
+  applyItems(shuffle(resources), "type", map.flat())
+);
+
+console.log(unflat(5, 5, temp));
